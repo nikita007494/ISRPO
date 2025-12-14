4f5df99f872c63a23e08b392c242313eb589095a
nikita007494 <61632144+nikita007494@users.noreply.github.com>
2025-12-14 23:23:12 +0300

    Add files via upload

---
+++ LABA1!/LABA_1/LABA_1/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace LABA_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Clear_button = new System.Windows.Forms.Button();
+            this.Resulit_button = new System.Windows.Forms.Button();
+            this.Add_button = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Tovar_comboBox = new System.Windows.Forms.ComboBox();
+            this.Resulit_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Clear_button
+            // 
+            this.Clear_button.Location = new System.Drawing.Point(251, 253);
+            this.Clear_button.Name = "Clear_button";
+            this.Clear_button.Size = new System.Drawing.Size(75, 23);
+            this.Clear_button.TabIndex = 0;
+            this.Clear_button.Text = "Очистить";
+            this.Clear_button.UseVisualStyleBackColor = true;
+            // 
+            // Resulit_button
+            // 
+            this.Resulit_button.Location = new System.Drawing.Point(458, 244);
+            this.Resulit_button.Name = "Resulit_button";
+            this.Resulit_button.Size = new System.Drawing.Size(75, 40);
+            this.Resulit_button.TabIndex = 1;
+            this.Resulit_button.Text = "Посчитать итог";
+            this.Resulit_button.UseVisualStyleBackColor = true;
+            // 
+            // Add_button
+            // 
+            this.Add_button.Location = new System.Drawing.Point(352, 186);
+            this.Add_button.Name = "Add_button";
+            this.Add_button.Size = new System.Drawing.Size(75, 23);
+            this.Add_button.TabIndex = 2;
+            this.Add_button.Text = "Добавить";
+            this.Add_button.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(332, 215);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Tovar_comboBox
+            // 
+            this.Tovar_comboBox.FormattingEnabled = true;
+            this.Tovar_comboBox.Location = new System.Drawing.Point(332, 147);
+            this.Tovar_comboBox.Name = "Tovar_comboBox";
+            this.Tovar_comboBox.Size = new System.Drawing.Size(121, 21);
+            this.Tovar_comboBox.TabIndex = 4;
+            // 
+            // Resulit_textBox
+            // 
+            this.Resulit_textBox.Location = new System.Drawing.Point(443, 327);
+            this.Resulit_textBox.Name = "Resulit_textBox";
+            this.Resulit_textBox.Size = new System.Drawing.Size(100, 20);
+            this.Resulit_textBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(278, 334);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Итоговая сумма за покупку";
+            // 
+            // Form1
+            // 
+
+            this.Add_button.Click += new System.EventHandler(this.Add_button_Click);
+            this.Clear_button.Click += new System.EventHandler(this.Clear_button_Click);
+            this.Resulit_button.Click += new System.EventHandler(this.Resulit_button_Click);
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Resulit_textBox);
+            this.Controls.Add(this.Tovar_comboBox);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Add_button);
+            this.Controls.Add(this.Resulit_button);
+            this.Controls.Add(this.Clear_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Clear_button;
+        private System.Windows.Forms.Button Resulit_button;
+        private System.Windows.Forms.Button Add_button;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox Tovar_comboBox;
+        private System.Windows.Forms.TextBox Resulit_textBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LABA1!/LABA_1/LABA_1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LABA_1
+{
+    public partial class Form1 : Form
+    {
+        // Подключение к базе данных supermarket
+        private string connectionString = "Data Source=DESKTOP-HKB5J94;Initial Catalog=Supermarket;Integrated Security=True";
+
+        // Словарь для хранения цен по названию продукта
+        private Dictionary<string, decimal> products = new Dictionary<string, decimal>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadProducts();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        // Загружаем продукты из базы данных в ComboBox
+        private void LoadProducts()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT name, price FROM products";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string name = reader.GetString(0);
+                    decimal price = reader.GetDecimal(1);
+                    products[name] = price;
+                    Tovar_comboBox.Items.Add(name);
+                }
+            }
+        }
+
+        // Добавить выбранный продукт в ListBox
+        private void Add_button_Click(object sender, EventArgs e)
+        {
+            if (Tovar_comboBox.SelectedItem != null)
+            {
+                string product = Tovar_comboBox.SelectedItem.ToString();
+                listBox1.Items.Add(product);
+            }
+            else
+            {
+                MessageBox.Show("Выберите продукт!");
+            }
+        }
+
+        // Очистить список
+        private void Clear_button_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Resulit_textBox.Clear();
+        }
+
+        // Посчитать итоговую сумму
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Resulit_button_Click(object sender, EventArgs e)
+        {
+            decimal total = 0;
+            foreach (var item in listBox1.Items)
+            {
+                string name = item.ToString();
+                if (products.ContainsKey(name))
+                    total += products[name];
+            }
+            Resulit_textBox.Text = $"Итого: {total:C}";
+        }
+    }
+}
